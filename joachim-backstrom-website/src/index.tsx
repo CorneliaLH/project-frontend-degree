ec9fe99d04f7c22956211a8c82b31ed246b592b8
Conrelia Harder <cornelialisaharder@gmail.com>
2023-01-19 14:56:46 +0100

    deploy

--- joachim-backstrom-website/src/index.tsx
+++ joachim-backstrom-website/src/index.tsx
@@ -21,7 +21,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/'>
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />}></Route>
