719fc2addbd6a9192d3b88de5e4cd1e6397339f9
Conrelia Harder <cornelialisaharder@gmail.com>
2023-02-09 10:21:28 +0100

    testing changes when running local dev

--- joachim-backstrom-website/src/services/ScheduleService.ts
+++ joachim-backstrom-website/src/services/ScheduleService.ts
@@ -1,6 +1,12 @@
 import axios from "axios";
+// import { URLTOUSE } from "..";
 import { IScheduleConcert } from "../models/IScheduleConcert";
 import { IScheduleOpera } from "../models/IScheduleOpera";
+
+export const URLTOUSE: any =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3001/"
+    : process.env.REACT_APP_BACKEND_URL;
 
 export class ScheduleService {
   ////////////////////////
@@ -10,9 +16,7 @@ export class ScheduleService {
   //Get first 3 future schedule posts by most current date
   async getSchedule() {
     try {
-      let response = await axios.get<any>(
-        process.env.REACT_APP_BACKEND_URL + "schedule"
-      );
+      let response = await axios.get(URLTOUSE + "schedule");
       return response.data;
     } catch (error) {
       console.log(error);
@@ -21,10 +25,10 @@ export class ScheduleService {
 
   //Get all future schedule posts
   async getScheduleAll() {
+    console.log(process.env.REACT_APP_BACKEND_URL);
+    console.log(URLTOUSE);
     try {
-      let response = await axios.get<any>(
-        process.env.REACT_APP_BACKEND_URL + "schedule/all"
-      );
+      let response = await axios.get(URLTOUSE + "schedule/all");
       return response.data;
     } catch (error) {
       console.log(error);
@@ -34,7 +38,7 @@ export class ScheduleService {
   //Get all past/future schedule posts
   async getScheduleTotal() {
     try {
-      let response = await axios.get<any>(
+      let response = await axios.get(
         process.env.REACT_APP_BACKEND_URL + "schedule/total"
       );
       return response.data;
@@ -50,7 +54,7 @@ export class ScheduleService {
   //Post schedule AND opera OR concert repertoire posts
   async postSchedule(schedulePost: IScheduleOpera | IScheduleConcert) {
     try {
-      let response = await axios.post<any>(
+      let response = await axios.post(
         process.env.REACT_APP_BACKEND_URL + "schedule/add",
         schedulePost,
 
@@ -68,7 +72,7 @@ export class ScheduleService {
 
   async deleteSchedule(scheduleId: string) {
     try {
-      let response = await axios.post<any>(
+      let response = await axios.post(
         process.env.REACT_APP_BACKEND_URL + "schedule/delete",
         { _id: scheduleId },
         { headers: { "Content-Type": "application/json" } }
@@ -85,7 +89,7 @@ export class ScheduleService {
 
   async changeSchedule(schedule: any) {
     try {
-      let response = await axios.post<any>(
+      let response = await axios.post(
         process.env.REACT_APP_BACKEND_URL + "schedule/change",
         { schedule },
         { headers: { "Content-Type": "application/json" } }
